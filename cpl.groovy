6090eb24bd1c54ccb162cbec5d248c901af2a782
jayabalandevops <balaniaf@gmail.com>
2019-12-12 17:47:46 +0530

    added groovy script cps.groovy and touched cit.groovy

--- cpl.groovy
+++ cpl.groovy
@@ -1,1 +1,27 @@
 folder("$BUName"){}
+folder("$BUName/$ProdectName"){}
+pipelineJob("$BUName/$ProductName/CICD_$AppName"){
+    parameters{
+        stringParam("AppRepo","$ApplicationRepo","Git URL")
+        stringParam("NodeName","$NodeName","")
+        stringParam("UnitTestTool","$UnitTestRun","")
+        stringParam("AppName","$AppName","")
+        activeChoiceParam('Branch'){
+            description('Select the branch')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript{
+                script('["select","master","develop"]')
+                fallbackScript('"fallback choice"')
+            }
+        }
+    }
+    definition{
+        cps{
+            def jobScript = readFileFromWorkspace('cit.groovy')
+            script(jobScript)
+            def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+            approvals.approveScript(approvals.hash(jobScript,"groovy"))
+        }
+    }
+}
